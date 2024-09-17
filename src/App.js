f5c9d60f2cd758e94d6d47e254e4d387aaf4c470
Umar rahman <umaryousufzai9@gmail.com>
2024-09-17 23:04:16 +0500

    btn-heading with 1,2,3 steps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import { AiFillCheckCircle } from "react-icons/ai"; // import a react icon
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  function nextHandler() {
+    if (counter < 3) {
+      setCounter(counter + 1);
+    }
+  }
+
+  function previousHandler() {
+    if (counter > 1) {
+      setCounter(counter - 1);
+    }
+  }
+
+  const steps = [
+    { id: 1, text: "Step 1: Learn React" },
+    { id: 2, text: "Step 2: Invest your new income" },
+    { id: 3, text: "Step 3: Final Step" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    <h1 style={{justifyContent: "center", textAlign: "center", background: "orange", padding:"5px"}}>Previous-Next btn with 3ree steps</h1>
+    <div style={{background: "orange", height: "500px" }}>
+      <div style={{ display: "flex", justifyContent: "space-around",}}>
+        <p
+          style={{
+            backgroundColor: counter === 1 ? "blue" : "white",
+            border: "1px solid black",
+            borderRadius: "50%",
+            padding: "5px 10px 5px 10px",
+            color: counter === 1 ? "white" : "black",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          1
+        </p>
+        <p
+          style={{
+            backgroundColor: counter === 2 ? "blue" : "white",
+            border: "1px solid black",
+            borderRadius: "50%",
+            padding: "5px 10px 5px 10px",
+            color: counter === 2 ? "white" : "black",
+          }}
+        >
+          2
+        </p>
+        <p
+          style={{
+            backgroundColor: counter === 3 ? "blue" : "white",
+            border: "1px solid black",
+            borderRadius: "50%",
+            padding: "5px 10px 5px 10px",
+            color: counter === 3 ? "white" : "black",
+          }}
+        >
+          3
+        </p>
+      </div>
+
+      {/* Dynamic heading */}
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <h1 style={{background: " white"}}>
+          {steps[counter - 1].text} 
+          {/* <AiFillCheckCircle /> */}
+        </h1>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "space-around", marginTop: "100px" }}>
+        <button style={{marginLeft: "20px", borderRadius: "20px", padding: "5px", backgroundColor: "blue", color:'white', cursor:"pointer"}} onClick={previousHandler}>Previous</button>
+        <button style={{marginLeft: "20px", borderRadius: "20px", padding: "5px", backgroundColor: "blue", color:'white', cursor:"pointer"}} onClick={nextHandler}>Next</button>
+      </div>
     </div>
+    </>
   );
 }
 
